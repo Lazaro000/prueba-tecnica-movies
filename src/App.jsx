e566dceb1f846ab2a920fac6464fd129d6ce9107
Lázaro <lazaroyaesta@gmail.com>
2022-08-01 00:37:46 +0200

    🧱 Add Reducer Folder & movies-search reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useReducer, useState } from 'react';
 import { searchTrendingMovies } from './lib/api/search-trending-movies';
+import {
+	moviesSearchReducer,
+	MOVIES_SEARCH_ACTIONS,
+	MOVIES_SEARCH_INITIAL_STATE
+} from './lib/reducers/movies-search.reducer';
 
 const App = () => {
 	const { movies, page, error, loading, setPage } = useMovies();
@@ -38,80 +43,36 @@ const searchTrending = async (
 	}
 };
 
-const ACTIONS = {
-	START_SEARCH: 0,
-	SEARCH_SUCCESS: 1,
-	SEARCH_ERROR: 2,
-	SET_PAGE: 3
-};
-
 // * Custom hook
 const useMovies = () => {
-	const moviesSearchReducer = (state, action) => {
-		switch (action.type) {
-			case ACTIONS.START_SEARCH:
-				return {
-					...state,
-					error: undefined,
-					loading: true
-				};
-
-			case ACTIONS.SEARCH_SUCCESS:
-				return {
-					...state,
-					movies: action.movies,
-					loading: false
-				};
-
-			case ACTIONS.SEARCH_ERROR:
-				return {
-					...state,
-					movies: undefined,
-					error: action.error,
-					loading: false
-				};
-
-			case ACTIONS.SET_PAGE:
-				return {
-					...state,
-					page: action.page
-				};
-
-			default:
-				throw new Error('Invalid action');
-		}
-	};
-
-	const [moviesSearch, setMoviesSearch] = useReducer(moviesSearchReducer, {
-		movies: undefined,
-		page: 1,
-		error: undefined,
-		loading: false
-	});
+	const [moviesSearch, setMoviesSearch] = useReducer(
+		moviesSearchReducer,
+		MOVIES_SEARCH_INITIAL_STATE
+	);
 
 	const startSearch = () => {
 		setMoviesSearch({
-			type: ACTIONS.START_SEARCH
+			type: MOVIES_SEARCH_ACTIONS.START_SEARCH
 		});
 	};
 
 	const searchSuccess = movies => {
 		setMoviesSearch({
-			type: ACTIONS.SEARCH_SUCCESS,
+			type: MOVIES_SEARCH_ACTIONS.SEARCH_SUCCESS,
 			movies
 		});
 	};
 
 	const searchError = error => {
 		setMoviesSearch({
-			type: ACTIONS.SEARCH_ERROR,
+			type: MOVIES_SEARCH_ACTIONS.SEARCH_ERROR,
 			error
 		});
 	};
 
 	const setPage = page => {
 		setMoviesSearch({
-			type: ACTIONS.SET_PAGE,
+			type: MOVIES_SEARCH_ACTIONS.SET_PAGE,
 			page
 		});
 	};
