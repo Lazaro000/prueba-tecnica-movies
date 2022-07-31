0ac5b9b9f5f992e407b890915c64b3649fe9819b
Lázaro <lazaroyaesta@gmail.com>
2022-08-01 01:16:23 +0200

    ✨ Movie card & movie DTO

---
+++ src/components/movie-card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { API_IMAGE_HOST } from '../constants/api';
+
+const MovieCard = ({ title, image, year, rating }) => {
+	return (
+		<div className='w-1/4 p-4'>
+			<div className=''>
+				<img
+					className='w-full block'
+					src={`${API_IMAGE_HOST}${image}`}
+				/>
+				<h2>{title}</h2>
+				<span>{rating}</span>
+				<span>{year}</span>
+			</div>
+		</div>
+	);
+};
+
+export default MovieCard;
