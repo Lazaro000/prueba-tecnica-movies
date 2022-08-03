a76887208f12a2ffe3928221960f6555d82ee832
Lázaro <lazaroyaesta@gmail.com>
2022-08-03 14:47:49 +0200

    💄 ✨ Pagination & Glamorous styles

--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { API_IMAGE_HOST } from '../constants/api';
-
 const MovieCard = ({ title, image, year, rating }) => {
 	return (
-		<div className='w-1/4 p-4'>
-			<div className=''>
-				<img
-					className='w-full block'
-					src={`${API_IMAGE_HOST}${image}`}
-				/>
-				<h2>{title}</h2>
-				<span className='mr-2'>{rating}</span>
-				<span>{year}</span>
+		<div className='w-1/5 p-4'>
+			<div
+				style={{
+					backgroundImage: `url(${image})`
+				}}
+				className='relative aspect-2/3 bg-cover bg-gray'
+			>
+				<h3 className='absolute bottom-0 w-full bg-bg-700 p-2'>
+					{title}
+				</h3>
+				<span className='absolute top-2 left-2 py-1 px-2 bg-primary'>
+					{rating}
+				</span>
+				<span className='absolute top-2 right-2 py-1 px-2 bg-bg-700'>
+					{year}
+				</span>
 			</div>
 		</div>
 	);
