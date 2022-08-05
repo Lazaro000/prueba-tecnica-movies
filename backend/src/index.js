9f08e17b4a6432dd02be97fc4cf39866b818a3c4
Lázaro <lazaroyaesta@gmail.com>
2022-08-05 17:53:24 +0200

    🧑‍💻 Simple proxy to TMDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,29 @@
-console.log("HOLA MUNDO");
+// const express = require("express");
+// const dotenv = require("dotenv");
+// const fetch = require("node-fetch");
+import express from "express";
+import { config as dotenvConfig } from "dotenv";
+import fetch from "node-fetch";
+
+dotenvConfig();
+
+const expressApp = express();
+
+expressApp.get("/peliculas", async (req, res) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_API_KEY}&page=1`,
+    {
+      method: "GET",
+    }
+  );
+
+  const data = await response.json();
+
+  res.send(data);
+});
+
+expressApp.listen(process.env.PORT, () =>
+  console.log(
+    `🚀 Servidor de Express levantado en el puerto http://localhost:${process.env.PORT}`
+  )
+);
